ba6b37bcf33ead0e2f50831190a1582d69eef344
Farruh Sadullayev <sadullayevf923@gmail.com>
2024-11-07 19:48:35 +0500

    xatoni togilladim

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import Farruh from "./Components/Farruh/Farruh"
+import Farruh from "./Components/Farruh/Farruh";
+import Ruxsora from "./Components/Ruxsora/Ruxsora";
+import Nozimjon from "./Components/Nozimjon/Nozimjon";
 
 export default function App() {
   return(
     <>
-     
-      <Farruh/>
+      <Farruh />
     </>
-  )
+  );
 }
