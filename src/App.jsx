24dbe477f65eb73b7bb39a905c60761122581615
Nozimjon <masteryt123@mail.ru>
2024-11-07 19:28:34 +0500

    stage N3 is complate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Ruxsora from "./Components/Ruxsora/Ruxsora";
+import Nozimjon from "./Components/Nozimjon/Nozimjon";
+
 export default function App() {
   return(
     <>
-      <Ruxsora/>
+      <Nozimjon />
     </>
   )
 }
--- src/Components/Nozimjon/Nozimjon.jsx
+++ src/Components/Nozimjon/Nozimjon.jsx
@@ -4,6 +4,7 @@ import n1 from './Img/1.png'
 import c1 from './Img/4.png'
 import c2 from './img/5.png'
 import logo from './Img/3.png'
+import check from './Img/7.png'
 
 import Navbar from './components/navbar'
 import WhyUsSection from './components/whyUsSection'
@@ -42,6 +43,23 @@ export default function Nozimjon() {
                 <div className="stage-2"><WhyUsSection /></div>
                 <div className="stage-3">
                     <h1 className="p1-se-1"><span className="p1-se-2">ИСКУССТВЕННЫЙ <br /> ИНТЕЛЕКТ</span> - ЭТО КРУТО?</h1>
+                    <div className="idk flex">
+                        <img src={ check } className='chImg' alt="check" />
+                        <p className='chText'>На данный момент только в Южной Корее ищут 10,000 специалистов в области AI!</p>
+                    </div>
+                    <div className="idk flex">
+                        <img src={ check } className='chImg' alt="check" />
+                        <p className='chText'>Младшие разработчики искусственного интеллекта без опыта работы могут получать в Южной Корее 60,000,000 вон (51,000$) в год. Специалист со средним стажем работы может получать уже около 100,000,000 вон (85,000$) в год.</p>
+                    </div>
+                    <div className="idk flex">
+                        <img src={ check } className='chImg' alt="check" />
+                        <p className='chText'>В США уровень зарплат ещё выше! Младший специалист без опыта работы получает в среднем 93,000$ в год. А инженер со средним стажем работы получает уже больше 140,000$ в год!</p>
+                    </div>
+                    <div className="idk flex">
+                        <img src={ check } className='chImg' alt="check" />
+                        <p className='chText'>А самое вкусное то, что твоя зарплата будет расти вместе с твоими навыками. Стаж не так важен.</p>
+                    </div>
+                    <button className='btn mt10'>Записаться на ознакомительный вебинар</button>
                 </div>
         </>
     )
