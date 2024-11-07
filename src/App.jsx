f465410bc63fa787cf100b3700dec7acfcade457
Nozimjon <masteryt123@mail.ru>
2024-11-07 20:21:01 +0500

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
+import { Route, Routes } from 'react-router-dom'
+
 import Ruxsora from "./Components/Ruxsora/Ruxsora";
 import Nozimjon from "./Components/Nozimjon/Nozimjon";
+import Diyorbek from "./Components/Diyorbek/Diyorbek/Diyorbek"
 
 export default function App() {
   return(
     <>
-      <Nozimjon />
+      <Routes>
+        <Route path="/" element={ <Nozimjon /> }/>
+        <Route path="/diyor" element={ <Diyorbek /> }/>
+        <Route path="/ruxsoo" element={ <Ruxsora /> }/>
+      </Routes>
     </>
   )
 }
--- src/Components/Nozimjon/components/navbar.jsx
+++ src/Components/Nozimjon/components/navbar.jsx
@@ -7,10 +7,10 @@ export default function Navbar() {
                     <p className='se-1'>control panel</p>
                     <ul>
                         <li><a href="">Asadbek</a></li>
-                        <li><a href="">Diyorbek</a></li>
-                        <li><a href="">Ruxsora</a></li>
+                        <li><a href="/diyor">Diyorbek</a></li>
+                        <li><a href="/ruxsoo">Ruxsora</a></li>
                         <li><a href="">Farruh</a></li>
-                        <li><a href="">Nozimjon</a></li>
+                        <li><a href="/">Nozimjon</a></li>
                     </ul>
                 </nav>
         </>
