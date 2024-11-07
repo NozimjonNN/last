d86c5603338c8325751ae7c11766f5ab8f13ad59
Farruh Sadullayev <sadullayevf923@gmail.com>
2024-11-07 19:51:47 +0500

    Resolve merge conflict and remove duplicate imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Farruh from "./Components/Farruh/Farruh";
-import Ruxsora from "./Components/Ruxsora/Ruxsora";
 import Nozimjon from "./Components/Nozimjon/Nozimjon";
 
 export default function App() {
-  return(
+  return (
     <>
       <Farruh />
     </>
